760ec64db0da2ff6739e0ac1a467a184c7934fa6
nidhi-salgaonkar <nidhisalgaonkar11@gmail.com>
2026-01-22 16:28:58 +0530

    Add files via upload

---
+++ assignment1_1.py
@@ -0,0 +1,12 @@
+num1=input("enter the first number:")
+num2=input("enter the second number:")
+
+add=float(num1)+float(num2)
+subtract=float(num1)-float(num2)
+multiply=float(num1)*float(num2)
+divide=float(num1)/float(num2)
+
+print("addition: ",add)
+print("subtraction: ",subtract)
+print("multiplication: ",multiply)
+print("division: ",divide)
